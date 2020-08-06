aa28e301e001f3b52e16e59634213de222765854
carezaza <carezaza@outlook.com>
2020-08-07 05:47:54 +0700

    'test'

---
+++ public/build/precache-manifest.20d95a89ffc9523346f04dff0df80510.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5995574710a325ed728440c6cd18995e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e21f9aa505d0a14c4e5e",
+    "url": "/static/css/main.ac78b0b5.chunk.css"
+  },
+  {
+    "revision": "7a4d31f8e7fccae208f1",
+    "url": "/static/js/2.0f4b3318.chunk.js"
+  },
+  {
+    "revision": "7baac2e68c562348b516173f26c107b7",
+    "url": "/static/js/2.0f4b3318.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e21f9aa505d0a14c4e5e",
+    "url": "/static/js/main.710ae8f5.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
